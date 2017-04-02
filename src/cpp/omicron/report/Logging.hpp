cda13d8d872a5af702ff87c37e8c902542749d58
David Saxon <davidesaxon@gmail.com>
2017-04-03 01:11:10 +1200

    build for windows 10

---
+++ src/cpp/omicron/report/Logging.hpp
@@ -0,0 +1,5 @@
+/*!
+ * \file
+ * \author David Saxon
+ * \brief Functionality related to logging through Omicron using ArcaneLog.
+ */
--- tools/w_build.bat
+++ tools/w_build.bat
@@ -1,5 +1,3 @@
-set VCTargetsPath=c:\Program Files (x86)\MSBuild\Microsoft.Cpp\v4.0\V120
-
 msbuild ..\Omicron.sln /v:n /p:Configuration=omicron_runtime
 msbuild ..\Omicron.sln /v:n /p:Configuration=omi_qt
 msbuild ..\Omicron.sln /v:n /p:Configuration=omi_gl
